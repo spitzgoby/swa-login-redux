33608bd9d757c779d82a10c01a3c7803083e1fb7
Thomas Leu <tomleu@gmail.com>
2019-09-06 07:28:42 -0500

    Fix the navbar colors and appearance

--- src/components/page/index.js
+++ src/components/page/index.js
@@ -1,19 +1,19 @@
-import AppBar from '@material-ui/core/AppBar'
-import Container from '@material-ui/core/Container'
-import Typography from '@material-ui/core/Typography'
-import React from 'react'
+import AppBar from '@material-ui/core/AppBar';
+import Container from '@material-ui/core/Container';
+import logo from './swa_logo_dark.png';
+import React from 'react';
 
-import './style.scss'
+import './style.scss';
 
 function Page(props) {
   return (
     <Container className="page">
       <AppBar className="navbar">
-        <Typography variant="h2">Southwest</Typography>
+        <img alt="Southwest" height="34" role="presentation" src={logo} width="223" />
       </AppBar>
-        {props.children}
+      {props.children}
     </Container>
-  )
+  );
 }
 
-export default Page
+export default Page;
